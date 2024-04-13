d2bcc4c3d67c38df181e911731aa990aac225bb5
Bryam Villalobos <bryamv@uninorte.edu.co>
2024-04-13 11:20:44 -0500

    lin updated

--- FrontEnd/StudentSchedule/src/components/Navegation.jsx
+++ FrontEnd/StudentSchedule/src/components/Navegation.jsx
@@ -13,7 +13,7 @@ function Navegation() {
     <>
       <Navbar expand="lg" className="bg-body-tertiary ">
         <Container>
-          <Navbar.Brand href="#home">Student Schedule</Navbar.Brand>
+          <Navbar.Brand href="/">Student Schedule</Navbar.Brand>
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="me-auto">
