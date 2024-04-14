e4968dfcddbe1bb35cd45adecef3586c246dcb94
Bryam Villalobos <bryamv@uninorte.edu.co>
2024-04-13 20:25:28 -0500

    shadcn added and updated hero section

--- FrontEnd/StudentSchedule/src/pages/HomePage.jsx
+++ FrontEnd/StudentSchedule/src/pages/HomePage.jsx
@@ -1,20 +1,9 @@
-import Container from "react-bootstrap/esm/Container";
-import Hero from "../components/Hero";
-import Row from "react-bootstrap/esm/Row";
-import Col from "react-bootstrap/esm/Col";
-import QuestionForm from "../components/QuestionForm";
+import { Hero } from "@/components/component/hero";
 
 function HomePage() {
   return (
     <>
       <Hero />
-      <Container>
-        <Row>
-          <Col>
-            <QuestionForm />
-          </Col>
-        </Row>
-      </Container>
     </>
   );
 }
--- FrontEnd/StudentSchedule/src/pages/Root.jsx
+++ FrontEnd/StudentSchedule/src/pages/Root.jsx
@@ -1,10 +1,8 @@
 import { Outlet } from "react-router-dom";
-import Navegation from "../components/Navegation";
 
 const Root = () => {
   return (
     <>
-      <Navegation />
       <Outlet />
     </>
   );
