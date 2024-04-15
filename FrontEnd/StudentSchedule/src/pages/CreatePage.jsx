15ab57f14218cd4400c8ffc6e54d907013c892c8
Bryam Villalobos <bryamv@uninorte.edu.co>
2024-04-15 15:47:10 -0500

    Create form and bug fixed

--- FrontEnd/StudentSchedule/src/pages/CreatePage.jsx
+++ FrontEnd/StudentSchedule/src/pages/CreatePage.jsx
@@ -1,9 +1,95 @@
-
+import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
 
 function CreatePage() {
+  const times = Array.from({ length: 48 }, (_, index) => {
+    const hour = Math.floor(index / 2);
+    const minute = index % 2 === 0 ? "00" : "30";
+    return `${hour.toString().padStart(2, "0")}:${minute}`;
+  });
   return (
-    <div>CreatePage</div>
-  )
+    <div className="flex justify-center items-center h-screen">
+      <Card className="w-[750px]">
+        <CardHeader>
+          <CardTitle className="text-3xl" >Crea tu plan de estudios</CardTitle>
+        </CardHeader>
+        <CardContent>
+          <form>
+            <div className="grid w-full items-center gap-4">
+              <div className="flex flex-col space-y-1.5">
+                <Label htmlFor="carrera">Carrera</Label>
+                <Input id="carrera" placeholder="Carrera" />
+              </div>
+              <div className="flex flex-col space-y-1.5">
+                <Label htmlFor="tema">Tema</Label>
+                <Input id="tema" placeholder="Tema" />
+              </div>
+              <div className="flex flex-col space-y-1.5">
+                <Label htmlFor="horas">Horas disponible a la semana</Label>
+                <Input
+                  id="horas"
+                  placeholder="Horas Disponible"
+                  type="number"
+                />
+              </div>
+
+              <div>
+                <Label htmlFor="hinicio" className="block text-left mb-2">
+                  Horario
+                </Label>
+
+                <div className="flex space-x-4 justify-center items-center">
+                  <Select>
+                    <SelectTrigger id="hinicio">
+                      <SelectValue placeholder="Hora Inicial" />
+                    </SelectTrigger>
+                    <SelectContent position="popper">
+                      {times.map((time) => (
+                        <SelectItem key={time} value={time}>
+                          {time}
+                        </SelectItem>
+                      ))}
+                    </SelectContent>
+                  </Select>
+                  <Select>
+                    <SelectTrigger id="hfinal">
+                      <SelectValue placeholder="Hora Final" />
+                    </SelectTrigger>
+                    <SelectContent position="popper">
+                      {times.map((time) => (
+                        <SelectItem key={time} value={time}>
+                          {time}
+                        </SelectItem>
+                      ))}
+                    </SelectContent>
+                  </Select>
+                </div>
+              </div>
+            </div>
+          </form>
+        </CardContent>
+        <CardFooter className="flex justify-end">
+          <Button>Crear</Button>
+        </CardFooter>
+      </Card>
+    </div>
+  );
 }
 
-export default CreatePage
+export default CreatePage;
--- FrontEnd/StudentSchedule/src/pages/ErrorPage.jsx
+++ FrontEnd/StudentSchedule/src/pages/ErrorPage.jsx
@@ -1,15 +1,15 @@
-import Navegation from "../components/Navegation";
+import { Navbar } from "@/components/component/navbar";
 
 const ErrorPage = () => {
   return (
     <>
-      <Navegation />
+      <Navbar />
       <div className="flex items-center justify-center h-screen">
         <main className="container">
           <div className="row">
             <div className="col-md-6 offset-md-3 text-center">
-              <h1 className="display-4">404</h1>
-              <h2 className="lead">Page not found</h2>
+              <h1 className="text-3xl font-bold tracking-tighter sm:text-4xl md:text-5xl lg:text-6xl/none">404</h1>
+              <h2 className="text-3xl tracking-tighter sm:text-4xl md:text-5xl lg:text-6xl/none">Page not found</h2>
             </div>
           </div>
         </main>
