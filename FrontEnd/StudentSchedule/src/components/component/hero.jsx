3db0e4b21783cac3688e764aaa2dcc75c86c1ac0
Bryam Anderson Villalobos De Leon <82421374+Bryamv@users.noreply.github.com>
2024-04-17 21:46:10 -0500

    nombre actualizado y ruta

--- FrontEnd/StudentSchedule/src/components/component/hero.jsx
+++ FrontEnd/StudentSchedule/src/components/component/hero.jsx
@@ -3,7 +3,7 @@ import { Button } from "../ui/button";
 import { Link } from "react-router-dom";
 export function Hero() {
   return (
-    <section className="w-full py-12 md:py-24 lg:py-32 xl:py-48 animate__animated animate__fadeIn bg-[url('C:\Users\R3\Desktop\student-schedule\FrontEnd\StudentSchedule\public\img\work-schedule-timetable-managing-workflow-organization-effective-work-scheduling-time-management-effective-time-spending-time-planning-concept-flat-modern-illustration-vector.jpg')]">
+    <section className="w-full py-12 md:py-24 lg:py-32 xl:py-48 animate__animated animate__fadeIn bg-[url('./img/fondo.jpg')]">
       <div className="container px-4 md:px-6 ">
         <div className="flex flex-col items-center space-y-4 text-center ">
           <div className="space-y-2">
